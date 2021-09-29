8a415255ef1dba784a25109d6730a1050a7cc0bf
no1_tx <no1@no1sg.ru>
2021-09-29 13:33:43 +0300

    simple bot protection

--- app.py
+++ app.py
@@ -2,16 +2,33 @@ import logging
 from aiohttp import web
 import os
 import sys
+from re import search
 
-app = web.Application()
 logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger()
 
 routes = web.RouteTableDef()
+
+bad_useragent_chunks = [
+    'bot', 'Bot', 'Crawler', 'crawl', 'Crawl',
+]
+
+
+@web.middleware
+async def main_middleware(request, handler):
+    for _ in bad_useragent_chunks:
+        if search(_, request.headers['User-Agent']):
+            return web.Response(status=403, text="Go away motherfucker.")
+    response = await handler(request)
+    return response
+
 
 if os.getenv('CONTENT_FOLDER'):
     routes.static('/', os.getenv('CONTENT_FOLDER'), show_index=True, follow_symlinks=True)
 else:
     routes.static('/', sys.argv[1], show_index=True, follow_symlinks=True)
+
+app = web.Application(middlewares=[main_middleware])
 
 app.add_routes(routes)
 
